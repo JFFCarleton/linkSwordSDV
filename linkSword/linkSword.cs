55ebab0c164e65e49f0363e062cdb6dad7897d3b
JFFCarleton <julienfriedfavreau@cmail.carleton.ca>
2020-05-14 16:05:12 -0400

    cleaned up the code some, moved a few things around, made plans for later.

--- linkSword/linkSword.cs
+++ linkSword/linkSword.cs
@@ -17,6 +17,7 @@ namespace linkSword
     public class linkSword : Mod{
         private int linkSwordID;
         private IJsonAssetsApi JsonAssets;
+        private Farmer player;
 
         //jsonAssets setup/////////////////////////////////////////////////////////////////////////////////////////////////////////
         
@@ -31,6 +32,7 @@ namespace linkSword
         private void GameLoop_SaveLoaded (object sender, SaveLoadedEventArgs e){
             if (JsonAssets != null) { linkSwordID = JsonAssets.GetWeaponId("linkSword");}
             if (linkSwordID == -1) { Monitor.Log("jsonAssts linkSwordID failed", LogLevel.Warn); }
+            this.player = Game1.player; //grab the player for calls later
         }
 
         //SMapi setup/////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -46,36 +48,16 @@ namespace linkSword
             //ensures that won't run unless the player is able to act, and they are using the "use tool" button.
             if (!Context.IsWorldReady || !Context.CanPlayerMove ||!e.Button.IsUseToolButton() ){ return; }
 
-            //ty kdau!
+            /*ty kdau!
             //1. Check if the current item is a MeleeWeapon, stops nulls too! <3
             //2. Compare the current wep to make sure you're holding the right thing!
-            if (Game1.player.CurrentItem is MeleeWeapon heldWep && heldWep.InitialParentTileIndex == linkSwordID && Game1.player.health == Game1.player.maxHealth)
-            {
-                Farmer player = Game1.player;
-
-                Game1.currentLocation.projectiles.Add(
-                    new BasicProjectile(
-                        0,                                                                  //int damageToFarmer
-                        1,                                                                  //int parentSheetIndex
-                        0,                                                                  //int bouncesTillDestruct
-                        0,                                                                  //int tailLength
-                        0.0f,                                                               //float rotationVelocity 
-                        -1.0f,                                                              //float xVelocity   
-                        -1.0f,                                                              //float yVelocity
-                        new Vector2(player.getStandingX()-16, player.getStandingY()-64-8),  //Vector2 startingPosition
-                        "",                                                                 //string collisionSound
-                        "",                                                                 //string firingSound
-                        false,                                                              //bool explode
-                        false,                                                              //bool damagesMonsters = false
-                        Game1.currentLocation,                                              //GameLocation location
-                        player,                                                             //Character firer
-                        false,                                                              //bool spriteFromObjectSheet
-                        null                                                                //onCollisionBehavior collisionBehavior
-                        )
-                    );
-     
-
-                    Monitor.Log("Pew Pew!", LogLevel.Warn);
+            //3. Check to see if player is at max HP
+            */
+            if (player.CurrentItem is MeleeWeapon heldWep && heldWep.InitialParentTileIndex == linkSwordID && player.health == player.maxHealth){
+                
+                //if yes, SHOOT SWORD BEAM!
+                Game1.currentLocation.projectiles.Add(new SwordBeam(player));
+                Monitor.Log("Pew Pew!", LogLevel.Warn);
            
             }else return;  
         }
@@ -87,3 +69,27 @@ namespace linkSword
         void LoadAssets(string path);
     }//end of interface IJasonAssetsApi
 }//end of namespace
+
+
+/*
+//feeling like could need you, might delete later, IDK
+Farmer player = Game1.player;
+new BasicProjectile(
+    0,                                                                  //int damageToFarmer
+    1,                                                                  //int parentSheetIndex
+    0,                                                                  //int bouncesTillDestruct
+    0,                                                                  //int tailLength
+    0.0f,                                                               //float rotationVelocity 
+    -1.0f,                                                              //float xVelocity   
+    -1.0f,                                                              //float yVelocity
+    new Vector2(player.getStandingX()-16, player.getStandingY()-64-8),  //Vector2 startingPosition
+    "",                                                                 //string collisionSound
+    "",                                                                 //string firingSound
+    false,                                                              //bool explode
+    false,                                                              //bool damagesMonsters = false
+    Game1.currentLocation,                                              //GameLocation location
+    player,                                                             //Character firer
+    false,                                                              //bool spriteFromObjectSheet
+    null                                                                //onCollisionBehavior collisionBehavior
+    )
+    */
---
+++ linkSword/swordBeam.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Projectiles;
+using StardewValley.Tools;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Xna.Framework;
+
+namespace linkSword
+{
+    class SwordBeam: BasicProjectile
+    {
+
+        public SwordBeam(Farmer player) //TODO: Make it shoot in the direction the player is slashing.
+            : base(     
+                    0,                                                                          //int damageToFarmer
+                    1,                                                                          //int parentSheetIndex
+                    0,                                                                          //int bouncesTillDestruct
+                    0,                                                                          //int tailLength
+                    0.0f,                                                                       //float rotationVelocity 
+                    -1.0f,                                                                      //float xVelocity   
+                    -1.0f,                                                                      //float yVelocity
+                    new Vector2(player.getStandingX() - 16, player.getStandingY() - 64 - 8),    //Vector2 startingPosition
+                    "",                                                                         //string collisionSound
+                    "",                                                                         //string firingSound
+                    false,                                                                      //bool explode
+                    false,                                                                      //bool damagesMonsters = false
+                    Game1.currentLocation,                                                      //GameLocation location
+                    player,                                                                     //Character firer
+                    false,                                                                      //bool spriteFromObjectSheet
+                    null                                                                        //onCollisionBehavior collisionBehavior
+                    )//end of base (or super) constructor 
+        {
+            /*
+             * Logic for animations will **probably** go here. Most everything else will **probably** be handeled by the BaiscProjectile part
+             *      -Tho that being said, maybe I should have the sprites tied to to this class?
+             *      -I supposed it'd be more modular if I had an assets file, and people could play with it.
+             */
+        }
+    }
+}
